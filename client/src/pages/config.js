108c881460c908eeb2c416f6c102fccdbe2d7625
root <root@ip-172-31-34-104.ap-south-1.compute.internal>
2025-08-12 05:12:48 +0000

    updated

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,5 +1,5 @@
 // const API_BASE_URL = "http://3.87.228.149:84";
-const API_BASE_URL = "http://3.80.143.36:84";
+const API_BASE_URL = "http://13.234.231.208:86";
 // export default API_BASE_URL;
 // const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://backend";
 /// export default API_BASE_URL;
